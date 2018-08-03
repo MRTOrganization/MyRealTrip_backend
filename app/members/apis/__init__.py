47f5d0e243b8dce35448d230fa61aff1f6cc3398
chrisaor <pchpch0070@gmail.com>
2018-08-04 08:43:35 +0900

    user_detail 구현

--- app/members/apis/__init__.py
+++ app/members/apis/__init__.py
@@ -1,1 +1,2 @@
 from .user_list import *
+from .user_detail import *
--- app/members/apis/user_detail.py
+++ app/members/apis/user_detail.py
@@ -1,11 +1,34 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from django.http import Http404
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from members.serializer import UserSerializer
 
 User = get_user_model()
 
-class UserDetail(generics.RetrieveAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserDetail(APIView):
+    def get_obejct(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_obejct(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_obejct(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        user = self.get_obejct(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
