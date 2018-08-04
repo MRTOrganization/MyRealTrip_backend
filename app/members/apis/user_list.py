8cff245fb259c4afc90d741524a63448f6cc0c2a
chrisaor <pchpch0070@gmail.com>
2018-08-04 10:31:15 +0900

    user_list APIView로 수정

--- app/members/apis/user_list.py
+++ app/members/apis/user_list.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from members.serializer import UserSerializer
@@ -7,8 +8,16 @@ from members.serializer import UserSerializer
 User = get_user_model()
 
 
-class UserList(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserList(APIView):
+    def get(self, request, format=None):
+        user = User.objects.all()
+        serializer = UserSerializer(user, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
