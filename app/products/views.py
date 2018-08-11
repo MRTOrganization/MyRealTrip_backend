8b39b86db86289b3be6d41fc315226ade0a8c748
chrisaor <pchpch0070@gmail.com>
2018-08-11 12:27:23 +0900

    product관련 뷰 수정

--- app/products/views.py
+++ app/products/views.py
@@ -5,12 +5,15 @@ from region.models import Country
 
 
 def popular_city_list(request):
-    # PopularCity 인스턴스를 생성
-    cities = PopularCity.objects.create()
-    # create_popular_city 클래스 메소드를 실행
-    cities.create_popular_city()
-    # PopularCity 객체를 전부 검색
-    popular_cities = PopularCity.objects.all()
+
+    if len(PopularCity.objects.all()) == 0:
+        cities = PopularCity.objects.create()
+        cities.create_popular_city()
+        popular_cities = PopularCity.objects.all()
+
+    else:
+        popular_cities = PopularCity.objects.all()
+
     context = {
         'popular_cities': popular_cities,
     }
