8b791b5ab46bbf2886d627210d33ef84cc0a5872
chrisaor <pchpch0070@gmail.com>
2018-08-11 17:20:27 +0900

    products api구현

--- app/config/urls/apis.py
+++ app/config/urls/apis.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('members/', include('members.urls.apis')),
     path('flights/', include('flights.urls.apis')),
+    path('products/', include('products.urls.apis')),
 ]
