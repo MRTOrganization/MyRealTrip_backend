4643625215ea8fabe1df6c8391cbf6800adf1430
ksw <zenuine1@gmail.com>
2018-08-12 15:40:35 +0900

    accomodations admin register 등록

--- app/accom_search/models.py
+++ app/accom_search/models.py
@@ -1,1 +1,8 @@
+from django.db import models
+
+
+class AccomInfo(models.Model):
+    attraction = models.CharField(max_length=200, blank=True)
+    depart_date = models.CharField(max_length=100, blank=True)
+    return_date = models.CharField(max_length=100, blank=True)
 
