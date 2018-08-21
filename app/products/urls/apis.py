d5d6b220b7631a30d31d2e09b4571b3da7c2678a
chrisaor <pchpch0070@gmail.com>
2018-08-21 23:24:44 +0900

    product category별로 6개씩 나오도록 api추가 구현

--- app/products/urls/apis.py
+++ app/products/urls/apis.py
@@ -5,7 +5,12 @@ from products import apis
 urlpatterns = [
     path('popular_city/', apis.PopularCityList.as_view()),
     path('product_list/', apis.ProductList.as_view()),
-    # path('product_search/<str:keyword>/', apis.ProductSearch.as_view()),
+    path('product_list/guides/', apis.ProductListGuide.as_view()),
+    path('product_list/tickets/', apis.ProductListTicket.as_view()),
+    path('product_list/activities/', apis.ProductListActivity.as_view()),
+    path('product_list/snapshot/', apis.ProductListSnapShot.as_view()),
+    path('product_list/fun/', apis.ProductListFun.as_view()),
+    path('product_list/convenience/', apis.ProductListConvenience.as_view()),
+    path('product_search/<str:keyword>/', apis.ProductSearch.as_view()),
     path('product_list/<str:country>/<str:city>/', apis.ProductDetail.as_view()),
-
 ]
